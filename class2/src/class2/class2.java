70ea870c0d994dd3e0e3591d541f8352793e7d5b
han <phk4029@naver.com>
2019-11-06 15:33:22 +0900

    asadasdasda

--- class2/src/class2/class2.java
+++ class2/src/class2/class2.java
@@ -3,7 +3,7 @@ package class2;
 public class class2 {
 
 	public static void main(String[] args) {
-		System.out.println("wonkwang univ sw computer department");
+		System.out.println("wonkwang univ sw computer department.");
 
 	}
 
