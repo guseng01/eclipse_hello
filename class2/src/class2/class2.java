926b82879b15a8db2318047d9c8bf564ef1829ab
han <phk4029@naver.com>
2019-11-06 16:16:20 +0900

    qweqwe

--- class2/src/class2/class2.java
+++ class2/src/class2/class2.java
@@ -4,7 +4,7 @@ public class class2 {
 
 	public static void main(String[] args) {
 	    System.out.println("wonkwang univ sw computer department");
-	    System.out.println("2nd grade");
+	    System.out.println("2nd grade.");
 	}
 
 }
