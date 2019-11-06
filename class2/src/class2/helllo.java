99ec49b4d2ee73682c4aedd0520aa0fde9d57487
han <phk4029@naver.com>
2019-11-06 11:30:16 +0900

    create

--- class2/src/class2/helllo.java
+++ class2/src/class2/helllo.java
@@ -3,7 +3,7 @@ package class2;
 public class helllo {
 
 	public static void main(String[] args) {
-		System.out.println("wonkwang univ department sw computer");
+		System.out.println("2nd grade");
 	
 	}
 
