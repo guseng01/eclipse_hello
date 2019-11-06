2783b2b59a8df0a249c6c7df023522d070305fb0
han <phk4029@naver.com>
2019-11-06 15:30:01 +0900

    created

--- class2/src/class2/class2.java
+++ class2/src/class2/class2.java
@@ -3,7 +3,7 @@ package class2;
 public class class2 {
 
 	public static void main(String[] args) {
-		System.out.println("wonkwang sw computer department");
+		System.out.println("2nd grade");
 
 	}
 
