a5f860a458d3b490cbe11f2cb9dac23d48629f67
han <phk4029@naver.com>
2019-11-06 11:15:25 +0900

    create

---
+++ hello/src/hello/class2.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("wonkwang univ department sw computer");
+	}
+
+}
