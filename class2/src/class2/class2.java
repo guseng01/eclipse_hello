6823d15d388d670c598fc0ef472c087544d626b3
han <phk4029@naver.com>
2019-11-06 11:21:54 +0900

    ddwdwer

--- class2/src/class2/class2.java
+++ class2/src/class2/class2.java
@@ -3,8 +3,8 @@ package class2;
 public class class2 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("2nd grade");
+		System.out.println("wonkwang univ department sw computer");
+	
 	
 	}
 
