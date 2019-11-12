0be008cc2c4d5b4a540002a810cb4176b8e28e53
han <phk4029@naver.com>
2019-11-12 17:05:27 +0900

    학년

--- class2/src/class2/class2.java
+++ class2/src/class2/class2.java
@@ -5,6 +5,7 @@ public class class2 {
 	public static void main(String[] args) {
 	    System.out.println("wonkwang univ sw computer department");
 	    System.out.println("2nd grade");
+	    System.out.println("ÇÐ¹ø 20162869");
 	}
 
 }
