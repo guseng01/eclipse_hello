61548d0eeb6b560b79cce394c1eb2a488a6810af
han <phk4029@naver.com>
2019-11-13 10:55:43 +0900

    이름

--- hello/src/hello/class1.java
+++ hello/src/hello/class1.java
@@ -5,7 +5,7 @@ public class class1 {
 	public static void main(String[] args) {
 		System.out.println("wonkwang univ sw computer department");
 		System.out.println("2nd grade");
-
+		System.out.println("이름 강구성");
 	}
 
 }
