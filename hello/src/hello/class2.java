6baa18e5d4a0d9613a360c89ce2f1fd1d3fcd90a
han <phk4029@naver.com>
2019-11-14 15:44:33 +0900

    원광대

--- hello/src/hello/class2.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-public class class2 {
-
-	public static void main(String[] args) {
-		System.out.println("wonkwang univ sw computer department");
-	}
-
-}
---
+++ hello/src/hello/class3.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class class3 {
+
+	public static void main(String[] args) {
+		System.out.println("wonkwang nuiv computer department");
+	}
+
+}
