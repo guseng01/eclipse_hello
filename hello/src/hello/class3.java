c73e412a5f7d4d76540f1a1ebc060020a93d46e6
han <phk4029@naver.com>
2019-11-14 15:52:08 +0900

    2nd

--- hello/src/hello/class3.java
+++ hello/src/hello/class3.java
@@ -4,7 +4,7 @@ public class class3 {
 
 	public static void main(String[] args) {
 		System.out.println("wonkwang nuiv computer department");
-		System.out.println("2nd grade");
+		System.out.println("2nd grade.");
 	}
 
 }
