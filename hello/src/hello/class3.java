53da015592ecf3bad490ced18c1a5d69fb2a5df2
han <phk4029@naver.com>
2019-11-14 15:58:20 +0900

    학번

--- hello/src/hello/class3.java
+++ hello/src/hello/class3.java
@@ -5,6 +5,7 @@ public class class3 {
 	public static void main(String[] args) {
 		System.out.println("wonkwang nuiv computer department");
 		System.out.println("2nd grade.");
+		System.out.println("ÇÐ¹ø 20162869");
 	}
 
 }
